506abfcecbbb570363f79af0e42e865b82d5c4b5
edward.jw <edward.jw@kakaocorp.com>
2018-11-20 15:35:12 +0900

    vue 스터디 업로드

---
+++ vueApp/src/component/Board.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    Board
+    {{bid}}
+    <div v-if="loading">
+      loading board...
+    </div>
+    <div v-else>
+      로딩 완료
+      <router-link :to="`/b/${bid}/c/1`">Card1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card2</router-link>
+    </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+const board = {
+  data(){
+    return{
+      bid:0,
+      loading:false
+    }
+  },
+  created(){
+    this.fetchData();
+  },
+  updated(){
+
+  },
+  methods : {
+    fetchData(){
+      this.loading = true;
+      setTimeout(()=>{
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      },500);
+    }
+  }
+}
+
+export default board
+
+</script>
+
+<style>
+
+</style>
